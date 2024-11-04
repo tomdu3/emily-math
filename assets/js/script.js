3a73c2d0455a2003bebaef617a8aafc218e7b7a7
Tomislav Dukez <tomdu3@ymail.com>
2024-11-04 12:10:53 +0100

    add subtraction 2 nums and random selection of operation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,15 @@ const operation = document.querySelector('#operation');
 const result = document.querySelector('#result');
 
 start.addEventListener('click', () => {
-    let [num1, num2, res, op] = addTwoNumbers();
+    let selectOperation, num1, num2, res, op;
+    selectOperation = Math.floor(Math.random() * 2);
+
+    if (selectOperation == 0) {
+        [num1, num2, res, op] = addTwoNumbers();
+    }
+    else {
+        [num1, num2, res, op] = subtractTwoNumbers();
+    }
 
     number1.innerHTML = num1;
     number2.innerHTML = num2;
@@ -14,8 +22,9 @@ start.addEventListener('click', () => {
 })
 
 function generateTwoNumbers() {
-    let num1 = Math.floor(Math.random() * 10);
-    let num2 = Math.floor(Math.random() * 90) + 10;
+    let num1 = Math.floor(Math.random() * 99 + 1);
+    let num2 = Math.floor(Math.random() * 10+1);
+
     return [num1, num2];
 }
 
@@ -28,5 +37,16 @@ function addTwoNumbers() {
     res = num1 + num2;
     op = "+";}
     while (res > 100);
+    return [num1, num2, res, op];
+}
+
+function subtractTwoNumbers() {
+    let res = 0;
+    let num1, num2, op;
+    do {
+    [num1, num2] = generateTwoNumbers();
+    res = num1 - num2;
+    op = "-";}
+    while (res < 0);
     return [num1, num2, res, op];
 }
